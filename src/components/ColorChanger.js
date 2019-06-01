4f3bf3c93e164c30d0f5b5933247c3517febef5b
sammarston15 <sammarston15@gmail.com>
2019-06-01 09:09:21 -0600

    Finished this project

--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 export default class ColorChanger extends Component {
-  render() {
+  render(props) {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" onChange={ (e) => props.update(e.target.value) } disabled={ props.allowEdit === "false" }>
         <option value="black"> Black </option>
         <option value="blue"> Blue </option>
         <option value="green"> Green </option>
--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -2,30 +2,31 @@ import React, { Component } from 'react';
 
 export default class TextContainer extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      text: '',
-    };
+      text: ''
+    }
+    this.updateText = this.updateText.bind(this)
   }
 
   updateText(e) {
     this.setState({
-      text: e.target.value,
-    });
+      text: e.target.value
+    })
   }
 
   render() {
     return (
       <div className="textContainer">
-        <textarea
-          // style={  }
+        <textarea 
+          style={ { color: this.props.fontColor, fontFamily: this.props.fontFamily, fontSize: this.props.fontSize } }
           onChange={this.updateText}
-          value={this.state.text}
-          placeholder="Start typing your thoughts here!"
+          value={this.state.text} 
+          placeholder='This is where your text will be!'
           cols="90"
-          rows="30"
-        />
+          rows="30">
+        </textarea>
       </div>
-    );
+    )
   }
 }
